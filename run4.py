969ca45452549fb2b50631106eb3bae3f7ca7acd
Linkinfan <linkinfan@bk.ru>
2021-05-21 17:30:22 +0500

    4 pages links added [ver.0.02]

--- run4.py
+++ run4.py
@@ -2,18 +2,71 @@ import collections,multiprocessing,time
 from selenium import webdriver
 #from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.keys import Keys
+from pprint import pprint
 
+linksNotClean = []
+links = []
 start_time = time.time();
-
 chromeOptions = webdriver.ChromeOptions() 
 chromeOptions.add_experimental_option('excludeSwitches', ['enable-logging'])
 chromeOptions.add_argument('--headless')
 driver = webdriver.Chrome(options=chromeOptions, executable_path=r'C:\drivers\chromedriver.exe')
 #action = ActionChains(driver)
+driver.get('https://zakaz.bashkortostan.ru/purchases/new')
+fulltime = round((time.time() - start_time),2)
+time.sleep(1)
+butt2 = driver.find_element_by_xpath("(//button[@aria-label='Go to page 2'])[1]")
+butt3 = driver.find_element_by_xpath("(//button[@aria-label='Go to page 3'])[1]")
+butt4 = driver.find_element_by_xpath("(//button[@aria-label='Go to page 4'])[1]")
 
-driver.get('https://zakaz.bashkortostan.ru/purchases/new');
+def get_links():
+    if links == []:
+        print('---Lets parse---')
+    webElements = driver.find_elements_by_partial_link_text('')  #all links on page    
+    time.sleep(1)
+    
+    for i in webElements:    
+        intermediate = i.get_attribute('href')
+        linksNotClean.append(intermediate)
 
-#time.sleep(3) # Let the user actually see something!
+    
+
+    for i in linksNotClean:
+        if "order-info" in i:
+            if i not in links:
+                links.append(i)
+
+    count = len(links)
+    print('%s links received' % count)
+    print('--------------------')
+get_links()
+
+butt2.click()
+time.sleep(2)
+
+get_links()
+
+butt3.click()
+time.sleep(2)
+
+get_links()
+
+butt4.click()
+time.sleep(2)
+
+get_links()
+
+
+
+#pprint(links)
+print('Worktime: %s seconds' % fulltime)
+
+driver.close()
+
+
+#element.get_attribute('innerHTML');
+
+# window_before = driver.window_handles[0]
 
 #element = driver.find_element_by_xpath("//div[@id='a']//a[@class='click']")
 # timetoend = driver.find_elements_by_class_name('fs-18 m-0 grey-color fw-600 time_to_end') #Осталось до окончания приема предложений
@@ -22,27 +75,3 @@ driver.get('https://zakaz.bashkortostan.ru/purchases/new');
 # procurementLinks = driver.find_elements_by_xpath('a.system_link-style') # ссылка на закупку system_link-style
 # #procurementLinks = driver.find_element_by_class_name('d-flex align-items-center green-color fs-12 fw-600 my-2') # ссылка на закупку system_link-style
 # proposalLinks = driver.find_element_by_class_name('d-flex align-items-center green-color fs-12 fw-600 my-2') # ссылка на закупку system_link-style
-
-fulltime = round((time.time() - start_time),2)
-#element.get_attribute('innerHTML');
-time.sleep(1)
-asdasda = driver.find_elements_by_partial_link_text('')  #Информация о закупке 
-print('---Lets parse---')
-time.sleep(1)
-linksNotClean = []
-links = []
-for i in asdasda:    
-    intermediate = i.get_attribute('href')
-    linksNotClean.append(intermediate)
-#print('--------------------')
-for i in linksNotClean:
-    if "order-info" in i:
-        if i not in links:
-            links.append(i)
-count = len(links)
-print('%s links received' % count)
-print('Worktime: %s seconds' % fulltime)
-
-# window_before = driver.window_handles[0]
-#time.sleep(3) # Let the user actually see something!
-driver.close()
