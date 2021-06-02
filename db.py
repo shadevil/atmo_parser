ece3a46b3ad89a34e671b325e47350755580e758
Linkinfan <linkinfan@bk.ru>
2021-06-02 17:16:30 +0500

    have to allow adding to database

--- db.py
+++ db.py
@@ -18,14 +18,16 @@ def create_connection():
         if conn:
             conn.close()
             
-def insert(db_file,links,dynamics,sections,dates,times,counts):
-    try:        
+def insert(db_file,list):
+    try:
+        print('Adding to database...')   
         conn = sqlite3.connect(db_file)        
         c = conn.cursor()
-        for i in range(len(links)):
-            command = "insert into tenders (link,dynamics,section,dates,times,counts) values(?,?,?,?,?,?)" #,section,dates,times,counts
-            c.execute(command,(links[1][i],sections[2,i],dates[1,i],times[1,i],counts[1,i]))        #section,dates[i],times[i],counts[i]
+        for i in range(len(list[0])):
+            command = "insert into tenders (link,section,dates,times,counts) values(?,?,?,?,?)" #,section,dates,times,counts
+            c.execute(command,(list[0][i],list[1][i],list[2][i],list[3][i],list[4][i]))        #section,dates[i],times[i],counts[i]
             conn.commit()
+            print('OK')
     except Error as e:
         print(e)
     finally:
